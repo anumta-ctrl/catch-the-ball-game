c8cbc211460e4309b38b1276f56e7294cd198bda
anumta-ctrl <anumta.adeel.1020@gmail.com>
2025-07-12 07:30:39 -0700

    Add files via upload

---
+++ service worker.js
@@ -0,0 +1,24 @@
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("v1").then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./icon.png",
+        "./sounds/catch.ogg",
+        "./sounds/bgmusic.mp3",
+        "./sounds/gameover.ogg",
+        "./sounds/levelup.ogg"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
